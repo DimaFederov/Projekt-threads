fe6ba703918462cac256c7527a8d2c0cfba8949c
DimaFederov <thekarthik2312@gmail.com>
2023-09-29 13:24:30 +0530

    adding existing to main

---
+++ threads-app/app/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
+import "./globals.css"
+
+const HeaderTop = () => {
+  return (
+    <div>
+      <style>
+        {`
+          body {
+            background-color: black;
+            color: white;
+            margin: 0; /* Remove default body margin */
+          }
+          .navbar {
+            background-color: black;
+            display: flex;
+            justify-content: center; /* Center content horizontally */
+            align-items: center;
+            height: 4rem; /* Set a fixed height for the navbar */
+            border-bottom: 1px solid white; /* Add a white border at the bottom */
+          }
+          .navbar-text {
+            font-size: 15px;
+          }
+        `}
+      </style>
+      <div className="navbar">
+        <div className="navbar-text">
+          <b>CONFESSIONS</b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderTop;
